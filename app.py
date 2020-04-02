380949f4947007311b1d3f4a5759455733be1014
Molly_888 <magcox5@yahoo.com>
2020-04-02 09:38:40 -0700

    Updated Combine_Csvs, Pandemic_data

--- app.py
+++ app.py
@@ -1,5 +1,23 @@
 from flask import Flask, jsonify
-import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Pandemic_data.db")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Pandemic = Base.classes.pandemics
+
 
 #################################################
 # Flask Setup
@@ -15,14 +33,32 @@ app = Flask(__name__)
 ### Load them into javascript 
 ### Query Pandemic grab it out of the database  
 
-@app.route("/api/v1.0/pandemic/swine")
-def swineFlu():
-    """Return the data as json"""
-    mergedsf = pd.read_csv("data/merged_swineflu_2019.csv")
-    mergeddata = mergedsf.to_dict(orient="records")
+@app.route("/api/v1.0/pandemic-data")
+def pandemic():
+   # Create our session (link) from Python to the DB
+    session = Session(engine)
 
-    return jsonify(mergeddata)
+    """ Query all Pandemics for 'Pandemic', 'Country', 'Year', 'Cases', 'Deaths', 'Lon', 'Lat', 'population'"""
+    results = session.query(Pandemic.Pandemic, Pandemic.Country, Pandemic.Year, Pandemic.Cases, Pandemic.Deaths, Pandemic.Lon, Pandemic.Lat, Pandemic.population).all()
 
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_pandemics = []
+    for Pandemic, Country, Year, Cases, Deaths, Lon, Lat, population in results:
+        pandemic_dict = {}
+        pandemic_dict["Pandemic"] = Pandemic
+        pandemic_dict["Country"] = Country
+        pandemic_dict["Year"] = Year
+        pandemic_dict["Cases"] = Cases
+        pandemic_dict["Deaths"] = Deaths
+        pandemic_dict["Lon"] = Lon
+        pandemic_dict["Lat"] = Lat
+        pandemic_dict["population"] = population
+
+        all_pandemics.append(pandemic_dict)
+
+    return jsonify(all_pandemics)
 
 @app.route("/")
 def welcome():
